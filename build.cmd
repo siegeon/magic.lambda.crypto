6c6dfcfd6de91b4183ab2453dd72221086da28dc
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 17:57:23 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -3,6 +3,5 @@ set version=%1
 set key=%2
 
 cd %~dp0
-
 dotnet build magic.lambda.crypto/magic.lambda.crypto.csproj --configuration Release --source https://api.nuget.org/v3/index.json
 dotnet nuget push magic.lambda.crypto/bin/Release/magic.lambda.crypto.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
