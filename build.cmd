e76490fe6ea4f50335b5189fcea1c47946bd6c16
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 16:25:43 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 cd %~dp0
-dotnet build magic.lambda.crypto/magic.lambda.crypto.csproj --configuration Release
+dotnet build magic.lambda.crypto/magic.lambda.crypto.csproj --configuration Release --source https://api.nuget.org/v3/index.json
