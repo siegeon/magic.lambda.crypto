107daee5a29bce36304c4c27656dce250e444982
Thomas Hansen <thomas@gaiasoul.com>
2020-10-19 19:47:06 +0300

    Increasing coverage

--- magic.lambda.crypto.tests/AesTests.cs
+++ magic.lambda.crypto.tests/AesTests.cs
@@ -3,6 +3,7 @@
  * See the enclosed LICENSE file for details.
  */
 
+using System;
 using System.Linq;
 using Xunit;
 
@@ -11,7 +12,7 @@ namespace magic.lambda.crypto.tests
     public class AesTests
     {
         [Fact]
-        public void EncryptDecrypt()
+        public void EncryptDecrypt128bits()
         {
             var lambda = Common.Evaluate(@"
 crypto.aes.encrypt:Howdy, this is cool
@@ -34,6 +35,28 @@ crypto.aes.encrypt:Howdy, this is cool
    password:abcdefghij123456abcdefghij123456
 crypto.aes.decrypt:x:-
    strength:256
+   password:abcdefghij123456abcdefghij123456
+");
+            Assert.NotEqual("Howdy, this is cool", lambda.Children.First().Value);
+            Assert.Equal("Howdy, this is cool", lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void EncryptDecrypt_Throws()
+        {
+            Assert.Throws<ArgumentException>(() => Common.Evaluate(@"
+crypto.aes.encrypt:Howdy, this is cool
+   strength:256
+"));
+        }
+
+        [Fact]
+        public void EncryptDecryptDefaultBits()
+        {
+            var lambda = Common.Evaluate(@"
+crypto.aes.encrypt:Howdy, this is cool
+   password:abcdefghij123456abcdefghij123456
+crypto.aes.decrypt:x:-
    password:abcdefghij123456abcdefghij123456
 ");
             Assert.NotEqual("Howdy, this is cool", lambda.Children.First().Value);
